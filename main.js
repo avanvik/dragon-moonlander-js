84ba2248fb461a7d1c78be0d4191bf87c9a7fdf8
ballexman <alexander.vanvik@gmail.com>
2019-02-11 13:07:32 +0100

    Add touch controls

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
-var el = document.getElementById('rocket')
-var cont = document.getElementById('container')
-var startingPoint = {
-	x: (cont.offsetWidth - el.offsetWidth) / 2,
-	y: cont.offsetHeight - el.offsetHeight - 30
+const rocketElement = document.getElementById('rocket')
+const containerElement = document.getElementById('container')
+const controlPad = document.getElementById("control-pad")
+const controlPadLines = {
+  "horisontal": document.getElementById("line-h"),
+  "vertical": document.getElementById("line-v")
+}
+const flameElementLeft = document.getElementById('thrust-flame--l')
+const flameElementRight = document.getElementById('thrust-flame--r')
+const outputEl = document.getElementById('output')
+
+function output (data) {
+  outputEl.innerHTML = data
 }
 
-var gravity = 0.04
-var thrust = 0.08
+const settings = {
+  startingPoint: {
+    x: (containerElement.offsetWidth - rocketElement.offsetWidth) / 2,
+    y: containerElement.offsetHeight - rocketElement.offsetHeight - 30
+  },
+  framerate: 60,
+  gravity: 0.04,
+  thrustMultiplier: 0.08,
+  drag: .995,
+  rotDrag: 0.95
+}
 
 var acc = {
   x: 0,
-  y: gravity
+  y: settings.gravity
 }
 
 var speed = {
@@ -19,97 +36,137 @@ var speed = {
 }
 
 var pos = {
-  x: startingPoint.x,
-  y: startingPoint.y
+  x: settings.startingPoint.x,
+  y: settings.startingPoint.y
 }
 
 var torque = 0
 var rotation = 0
 
-var steering = {
-  up: 0,
-  down: 0,
-  left: 0,
-  right: 0
+var controls = {
+  horisontal: 0, // -1 to 1
+  vertical: 0,
+}
+
+function renderControlPad() {
+  controlPadLines.vertical.style.left = (controls.horisontal+1) / 2 * controlPad.offsetWidth -2 + "px"
+  controlPadLines.horisontal.style.bottom = controls.vertical * controlPad.offsetHeight -2 + "px"
 }
 
 document.addEventListener("keydown", event => {
   if (event.isComposing || event.keyCode === 38) {
-    steering.up = 1
+    controls.vertical = 1
   }
   if (event.isComposing || event.keyCode === 37) {
-    steering.left = 1
+    controls.horisontal = -1
   }
   if (event.isComposing || event.keyCode === 39) {
-    steering.right = 1
+    controls.horisontal = 1
   }
 });
 
 document.addEventListener("keyup", event => {
   if (event.isComposing || event.keyCode === 38) {
-    steering.up = 0
+    controls.vertical -= 1
   }
   if (event.isComposing || event.keyCode === 37) {
-    steering.left = 0
+    controls.horisontal += 1 
   }
   if (event.isComposing || event.keyCode === 39) {
-    steering.right = 0
+    controls.horisontal -= 1
   }
 });
 
-var lastTime = 0
-var dTime = 0
-function calcShip (timestamp) {
-  dTime = timestamp - lastTime
-  console.log(dTime)
-  
-  if (steering.up > 0) {
-    el.classList.add('thrusting')
+// Mouse
+controlPad.addEventListener('mousemove', (e)=>{
+  controls.horisontal = (e.offsetX / controlPad.offsetWidth) * 2 - 1
+  controls.vertical = 1 - e.offsetY / controlPad.offsetHeight
+  e.preventDefault()
+})
+
+controlPad.addEventListener('mouseout', (e)=>{
+  controls.horisontal = 0
+  controls.vertical = 0
+  e.preventDefault()
+})
+
+// Touch
+controlPad.addEventListener('touchmove', (e)=>{
+  const rect = e.target.getBoundingClientRect()
+  controls.horisontal = ((e.targetTouches[0].pageX - rect.left) / controlPad.offsetWidth) * 2 - 1
+  controls.vertical = 1 - (e.targetTouches[0].pageY - rect.top) / controlPad.offsetHeight
+  e.preventDefault()
+})
+
+controlPad.addEventListener('touchend', (e)=>{
+  controls.horisontal = 0
+  controls.vertical = 0
+  e.preventDefault()
+})
+
+function steerShip(){
+  if (controls.vertical > 0) {
+    rocketElement.classList.add('thrusting')
   } else {
-    el.classList.remove('thrusting')
+    rocketElement.classList.remove('thrusting')
   }
+
+  // Set fire lengths
+  flameElementLeft.style.height = controls.vertical * 20 + controls.horisontal * 10 + "px"
+  flameElementRight.style.height = controls.vertical * 20 + controls.horisontal * -10 + "px"
   
-  speed.x *= .995
-  speed.y *= .995
+  // Do physics stuff
+  speed.x *= settings.drag
+  speed.y *= settings.drag
   
-  torque += (steering.right - steering.left) * 0.2
-  torque *= 0.95
+  torque += controls.horisontal * 0.2
+  torque *= settings.rotDrag
   
-  acc.x = Math.sin(rotation * (Math.PI / 180)) * steering.up * thrust
-  acc.y = gravity - (steering.up * thrust * Math.cos(rotation * (Math.PI / 180)))
+  acc.x = Math.sin(rotation * (Math.PI / 180)) * controls.vertical * settings.thrustMultiplier
+  acc.y = settings.gravity - (controls.vertical * settings.thrustMultiplier * Math.cos(rotation * (Math.PI / 180)))
   
   speed.x += acc.x
   speed.y += acc.y
   
   pos.x += speed.x
   pos.y += speed.y
-  
-  if (pos.y + el.offsetHeight + 5 >= container.offsetHeight) {
+
+  rotation += torque
+}
+
+function checkCollisions(){
+  if (pos.y + rocketElement.offsetHeight + 5 >= container.offsetHeight) {
 		torque *= 0.2
     acc.y = 0
     speed.y = -0.5 * Math.abs(speed.y)
 		speed.x *= .2
-    pos.y = container.offsetHeight - (el.offsetHeight + 5)
+    pos.y = container.offsetHeight - (rocketElement.offsetHeight + 5)
+
   } else if (pos.x <= 0) {
     acc.x = 0
     speed.x = 0.5 * Math.abs(speed.x)
     pos.x = 0
-  } else if (pos.x + el.offsetWidth >= container.offsetWidth) {
+
+  } else if (pos.x + rocketElement.offsetWidth >= container.offsetWidth) {
     acc.x = 0
     speed.x = -0.5 * Math.abs(speed.x)
-    pos.x = container.offsetWidth - el.offsetWidth
+    pos.x = container.offsetWidth - rocketElement.offsetWidth
   }
-	
-	rotation += torque
-  
-  el.style.transform = `rotate(${rotation}deg)`
-  el.style.left = pos.x + 'px'
-  el.style.top = pos.y + 'px'
-  
-  lastTime = timestamp
-  window.requestAnimationFrame(calcShip)
 }
 
-window.requestAnimationFrame(calcShip)
-/* setInterval(calcShip, 5) */
+function moveShip() {
+  rocketElement.style.transform = `rotate(${rotation}deg)`
+  rocketElement.style.left = pos.x + 'px'
+  rocketElement.style.top = pos.y + 'px'
+}
+
+// ---
+
+function renderShip () {
+  steerShip(controls)
+  renderControlPad()
+  checkCollisions()
+  moveShip()
+}
+setInterval(renderShip, 1000/settings.framerate)
 
